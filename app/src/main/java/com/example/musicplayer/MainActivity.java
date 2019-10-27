e6bbc4c35010d5b45faf07c036b8851e8e3ef54e
Ryan Chung <ryan.chung851@gmail.com>
2019-10-27 02:55:59 -0400

    made notification ongoing and created intent for notification click to go to app

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -3,30 +3,27 @@ import static com.example.musicplayer.Notifications.CHANNEL_ID_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import androidx.media.app.NotificationCompat.MediaStyle;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
     Button play, pause, stop;
     MediaPlayer mediaPlayer;
     int pauseCurrentPosition;
-    private NotificationManagerCompat mNotificationManagerCompat;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
 
-        mNotificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManager = NotificationManagerCompat.from(this);
     }
 
     @Override
@@ -79,18 +76,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void sendNotification(View view){
         Bitmap largeImage = BitmapFactory.decodeResource(getResources(), R.drawable.music_default);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
         Notification channel1 = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID_1)
                 .setSmallIcon(R.drawable.ic_music)
-                .setContentTitle("song2")
-                .setContentText("artist2")
+                .setContentTitle("song3")
+                .setContentText("artist3")
                 .setLargeIcon(largeImage)
                 .addAction(R.drawable.ic_prev, "prev", null)
                 .addAction(R.drawable.ic_pause, "pause", null)
                 .addAction(R.drawable.ic_next, "next", null)
-                .setStyle(new MediaStyle().
-                        setShowActionsInCompactView(0, 2))
+                .setOngoing(true)
+                .setContentIntent(contentIntent)
+                .setStyle(new MediaStyle()
+                        .setShowActionsInCompactView(0, 2))
                 .build();
-        mNotificationManagerCompat.notify(1, channel1);
+        notificationManager.notify(1, channel1);
     }
 }

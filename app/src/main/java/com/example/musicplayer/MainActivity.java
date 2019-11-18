9f8557532b2a35d2642c9a5f6a1e4932e6296132
Ryan Chung <ryan.chung851@gmail.com>
2019-11-18 02:13:15 -0500

    implemented audiofocus handling for different cases

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -3,8 +3,13 @@ import static com.example.musicplayer.Notifications.CHANNEL_ID_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.PowerManager;
@@ -21,17 +26,26 @@ import android.app.Notification;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity
+        extends AppCompatActivity
+        implements View.OnClickListener{
     Button play, pause, stop;
     MediaPlayer mediaPlayer;
+    Context mContext;
+    AudioManager mAudioManager;
+    AudioAttributes mAudioAttributes;
+    AudioFocusRequest mAudioFocusRequest;
+    boolean mPlayOnAudioFocus;
     int pauseCurrentPosition;
     private MediaSessionCompat mediaSession;
     private NotificationManagerCompat notificationManager;
 
     @Override
+    @TargetApi(26)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
 
         play = findViewById((R.id.btn_play));
         pause = findViewById((R.id.btn_pause));
@@ -43,17 +57,72 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mediaSession = new MediaSessionCompat(this, "media");
         notificationManager = NotificationManagerCompat.from(this);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+
+        mAudioAttributes =
+                new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build();
+        mAudioFocusRequest =
+                new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                        .setAudioAttributes(mAudioAttributes)
+                        .setAcceptsDelayedFocusGain(true)
+                        .setOnAudioFocusChangeListener(
+                            new AudioManager.OnAudioFocusChangeListener() {
+                                @Override
+                                public void onAudioFocusChange(int focusChange) {
+                                    switch (focusChange) {
+                                        case AudioManager.AUDIOFOCUS_GAIN:
+                                            if (mPlayOnAudioFocus && !mediaPlayer.isPlaying()) {
+                                                mediaPlayer.seekTo(pauseCurrentPosition);
+                                                mediaPlayer.start();
+                                            } else if (mediaPlayer.isPlaying()) {
+                                                mediaPlayer.setVolume(1.0f, 1.0f);
+                                            }
+                                            mPlayOnAudioFocus = false;
+                                            break;
+                                        // case may not be necessary as Android O and above supports auto ducking automatically
+                                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                                            mediaPlayer.setVolume(0.05f, 0.05f);
+                                            break;
+                                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                                            if (mediaPlayer.isPlaying()) {
+                                                mPlayOnAudioFocus = true;
+                                                mediaPlayer.pause();
+                                                pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                                            }
+                                            break;
+                                        case AudioManager.AUDIOFOCUS_LOSS:
+                                            mAudioManager.abandonAudioFocus(this);
+                                            mPlayOnAudioFocus = false;
+                                            mediaPlayer.stop();
+                                            break;
+                                    }
+                                }
+                        }).build();
     }
 
     @Override
+    @TargetApi(26)
     public void onClick(View view){
 
         switch(view.getId()){
             case R.id.btn_play:
                 if (mediaPlayer == null) {
-                    mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.aft);
+                    mediaPlayer = MediaPlayer.create(mContext, R.raw.aft);
                     // keep CPU from sleeping and play music with screen off
                     mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+                    int focusRequest = mAudioManager.requestAudioFocus(mAudioFocusRequest);
+                    switch (focusRequest) {
+                        case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
+                            break;
+                        case AudioManager.AUDIOFOCUS_REQUEST_GRANTED:
+                            mediaPlayer.start();
+                            break;
+                    }
                 }
 
                 else if (!mediaPlayer.isPlaying()){

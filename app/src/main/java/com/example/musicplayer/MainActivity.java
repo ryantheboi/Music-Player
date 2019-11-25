07f56663071d29e8ef1a5da2e38be8fbc199ac0e
Ryan Chung <ryan.chung851@gmail.com>
2019-11-25 00:23:54 -0500

    changed mediaplayer to function as a service

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -5,14 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioAttributes;
-import android.media.AudioFocusRequest;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.PowerManager;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.view.View;
 import android.widget.Button;
@@ -25,18 +20,14 @@ import androidx.media.app.NotificationCompat.MediaStyle;
 import android.app.Notification;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.widget.Toast;
 
 public class MainActivity
         extends AppCompatActivity
         implements View.OnClickListener{
-    Button play, pause, stop;
-    MediaPlayer mediaPlayer;
-    Context mContext;
-    AudioManager mAudioManager;
-    AudioAttributes mAudioAttributes;
-    AudioFocusRequest mAudioFocusRequest;
-    boolean mPlayOnAudioFocus;
-    int pauseCurrentPosition;
+
+    private Button play, pause, stop;
+    private Intent serviceIntent;
     private MediaSessionCompat mediaSession;
     private NotificationManagerCompat notificationManager;
 
@@ -45,8 +36,6 @@ public class MainActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mContext = getApplicationContext();
-
         play = findViewById((R.id.btn_play));
         pause = findViewById((R.id.btn_pause));
         stop = findViewById((R.id.btn_stop));
@@ -55,54 +44,10 @@ public class MainActivity
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
 
+        serviceIntent = new Intent(this, MusicPlayerService.class);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
         mediaSession = new MediaSessionCompat(this, "media");
         notificationManager = NotificationManagerCompat.from(this);
-        setVolumeControlStream(AudioManager.STREAM_MUSIC);
-
-        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
-
-        mAudioAttributes =
-                new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_MEDIA)
-                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                        .build();
-        mAudioFocusRequest =
-                new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                        .setAudioAttributes(mAudioAttributes)
-                        .setAcceptsDelayedFocusGain(true)
-                        .setOnAudioFocusChangeListener(
-                            new AudioManager.OnAudioFocusChangeListener() {
-                                @Override
-                                public void onAudioFocusChange(int focusChange) {
-                                    switch (focusChange) {
-                                        case AudioManager.AUDIOFOCUS_GAIN:
-                                            if (mPlayOnAudioFocus && !mediaPlayer.isPlaying()) {
-                                                mediaPlayer.seekTo(pauseCurrentPosition);
-                                                mediaPlayer.start();
-                                            } else if (mediaPlayer.isPlaying()) {
-                                                mediaPlayer.setVolume(1.0f, 1.0f);
-                                            }
-                                            mPlayOnAudioFocus = false;
-                                            break;
-                                        // case may not be necessary as Android O and above supports auto ducking automatically
-                                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                                            mediaPlayer.setVolume(0.05f, 0.05f);
-                                            break;
-                                        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                                            if (mediaPlayer.isPlaying()) {
-                                                mPlayOnAudioFocus = true;
-                                                mediaPlayer.pause();
-                                                pauseCurrentPosition = mediaPlayer.getCurrentPosition();
-                                            }
-                                            break;
-                                        case AudioManager.AUDIOFOCUS_LOSS:
-                                            mAudioManager.abandonAudioFocus(this);
-                                            mPlayOnAudioFocus = false;
-                                            mediaPlayer.stop();
-                                            break;
-                                    }
-                                }
-                        }).build();
     }
 
     @Override
@@ -111,40 +56,15 @@ public class MainActivity
 
         switch(view.getId()){
             case R.id.btn_play:
-                if (mediaPlayer == null) {
-                    mediaPlayer = MediaPlayer.create(mContext, R.raw.aft);
-                    // keep CPU from sleeping and play music with screen off
-                    mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-                    int focusRequest = mAudioManager.requestAudioFocus(mAudioFocusRequest);
-                    switch (focusRequest) {
-                        case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
-                            break;
-                        case AudioManager.AUDIOFOCUS_REQUEST_GRANTED:
-                            mediaPlayer.start();
-                            break;
-                    }
-                }
-
-                else if (!mediaPlayer.isPlaying()){
-                    mediaPlayer.seekTo(pauseCurrentPosition);
-                    mediaPlayer.start();
-                }
-
+                startService(serviceIntent);
                 break;
 
             case R.id.btn_pause:
-                if (mediaPlayer != null) {
-                    mediaPlayer.pause();
-                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
-
-                }
+                startService(serviceIntent);
                 break;
 
             case R.id.btn_stop:
-                if (mediaPlayer != null) {
-                    mediaPlayer.stop();
-                    mediaPlayer = null;
-                }
+                stopService(serviceIntent);
                 break;
         }
     }
---
+++ app/src/main/java/com/example/musicplayer/MusicPlayerService.java
@@ -0,0 +1,162 @@
+package com.example.musicplayer;
+
+import android.annotation.TargetApi;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnErrorListener;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+public class MusicPlayerService
+        extends Service
+        implements OnCompletionListener, OnErrorListener {
+
+    private MediaPlayer mediaPlayer;
+    AudioManager mAudioManager;
+    AudioAttributes mAudioAttributes;
+    AudioFocusRequest mAudioFocusRequest;
+    boolean mPlayOnAudioFocus;
+    int pauseCurrentPosition;
+
+
+    @Override
+    @TargetApi(26)
+    public void onCreate() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.aft);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+
+        mAudioAttributes =
+                new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                        .build();
+        mAudioFocusRequest =
+                new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                        .setAudioAttributes(mAudioAttributes)
+                        .setAcceptsDelayedFocusGain(true)
+                        .setOnAudioFocusChangeListener(
+                                new AudioManager.OnAudioFocusChangeListener() {
+                                    @Override
+                                    public void onAudioFocusChange(int focusChange) {
+                                        switch (focusChange) {
+                                            case AudioManager.AUDIOFOCUS_GAIN:
+                                                if (mPlayOnAudioFocus && !mediaPlayer.isPlaying()) {
+                                                    mediaPlayer.seekTo(pauseCurrentPosition);
+                                                    mediaPlayer.start();
+                                                } else if (mediaPlayer.isPlaying()) {
+                                                    mediaPlayer.setVolume(1.0f, 1.0f);
+                                                }
+                                                mPlayOnAudioFocus = false;
+                                                break;
+                                            // case may not be necessary as Android O and above supports auto ducking automatically
+                                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                                                mediaPlayer.setVolume(0.05f, 0.05f);
+                                                break;
+                                            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                                                if (mediaPlayer.isPlaying()) {
+                                                    mPlayOnAudioFocus = true;
+                                                    mediaPlayer.pause();
+                                                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                                                }
+                                                break;
+                                            case AudioManager.AUDIOFOCUS_LOSS:
+                                                mAudioManager.abandonAudioFocus(this);
+                                                mPlayOnAudioFocus = false;
+                                                stopMedia();
+                                                break;
+                                        }
+                                    }
+                                }).build();
+
+    }
+
+    @TargetApi(26)
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // keep CPU from sleeping and play music with screen off
+        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        int focusRequest = mAudioManager.requestAudioFocus(mAudioFocusRequest);
+        switch (focusRequest) {
+            case AudioManager.AUDIOFOCUS_REQUEST_FAILED:
+                break;
+            case AudioManager.AUDIOFOCUS_REQUEST_GRANTED:
+                toggleMedia();
+                break;
+        }
+
+        // Start Stick return means service will explicitly continue until user
+        // ends it
+        return START_STICKY;
+
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+        }
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mp, int what, int extra) {
+        switch (what) {
+            case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+                Toast.makeText(this,
+                        "Media Error: Not valid for Progressive Playback " + extra,
+                        Toast.LENGTH_SHORT).show();
+            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+                Toast.makeText(this, "Media Error: Server Died!! " + extra,
+                        Toast.LENGTH_SHORT).show();
+            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+                Toast.makeText(this, "Media Error: Unknown " + extra,
+                        Toast.LENGTH_SHORT).show();
+
+        }
+
+        return false;
+    }
+
+
+    private void toggleMedia() {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.seekTo(pauseCurrentPosition);
+            mediaPlayer.start();
+        }
+        else if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+        }
+
+    }
+
+    private void stopMedia() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        stopMedia();
+        stopSelf();
+
+    }
+}

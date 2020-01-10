d9c444d35eec14b3a05fe6612b977a9f1ad88b93
Ryan Chung <ryan.chung851@gmail.com>
2020-01-10 15:49:10 -0500

    created ViewHolder static class for efficient ListView loading

--- app/src/main/java/com/example/musicplayer/SongListAdapter.java
+++ app/src/main/java/com/example/musicplayer/SongListAdapter.java
@@ -14,6 +14,15 @@ public class SongListAdapter extends ArrayAdapter {
     private Context mContext;
     private int mResource;
 
+    /**
+     * Holds variables in a View
+     */
+    static class ViewHolder{
+        TextView title;
+        TextView artist;
+        TextView album;
+    }
+
     public SongListAdapter(Context context, int resource, ArrayList<Song> objects) {
         super(context, resource, objects);
         mContext = context;
@@ -29,16 +38,26 @@ public class SongListAdapter extends ArrayAdapter {
         int albumID = ((Song) getItem(position)).getAlbumID();
         Song song = new Song(title, artist, album, albumID);
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        convertView = inflater.inflate(mResource, parent, false);
+        ViewHolder holder;
 
-        TextView tvTitle = convertView.findViewById(R.id.textView1);
-        TextView tvArtist = convertView.findViewById(R.id.textView2);
-        TextView tvAlbum = convertView.findViewById(R.id.textView3);
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource, parent, false);
 
-        tvTitle.setText(title);
-        tvArtist.setText(artist);
-        tvAlbum.setText(album);
+            holder = new ViewHolder();
+            holder.title = convertView.findViewById(R.id.textView1);
+            holder.artist = convertView.findViewById(R.id.textView2);
+            holder.album = convertView.findViewById(R.id.textView3);
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.title.setText(song.getTitle());
+        holder.artist.setText(song.getArtist());
+        holder.album.setText(song.getAlbum());
 
         return convertView;
     }
